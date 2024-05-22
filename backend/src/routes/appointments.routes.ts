b6ffbba498689a997ea57924cbfa9fe809e4e380
NilSousa16 <nil.sousa16@gmail.com>
2024-05-21 23:13:37 -0300

    Updating routes and creating services

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { startOfHour, parseISO } from 'date-fns';
+import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 const appointmentRepository = new AppointmentsRepository();
@@ -13,24 +14,24 @@ appointmentsRouter.get('/', (request, response) => {
 })  
 
 appointmentsRouter.post('/', (request,response) => {
-    const { provider, date } = request.body;
+    try {
+        const { provider, date } = request.body;
 
-    const parseDate = startOfHour(parseISO(date));
+        const parseDate = parseISO(date);
+        
+        const createAppointment = new CreateAppointmentService(
+            appointmentRepository,
+        );
 
-    const findAppointmentInSameDate = appointmentRepository.findByDate(parseDate);
+        const appointment = createAppointment.execute({ 
+            date: parseDate, 
+            provider,
+        });
 
-    if(findAppointmentInSameDate) {
-        return response
-            .status(400)
-            .json({ message: 'This appointment is already booked' });
+        return response.json(appointment);
+    } catch (err: any) {
+        return response.status(400).json({ error: err.message })
     }
-
-    const appointment = appointmentRepository.create({
-        provider, 
-        date: parseDate
-    });
-
-    return response.json(appointment);
 });
 
 export default appointmentsRouter;
